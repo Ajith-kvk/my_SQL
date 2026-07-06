cb23ea515fb532df9959ffa4f17745c3e03f369f
Ajith Kumar Vishwakarma <akvk55555@gmail.com>
2026-07-06 17:58:50 +0530

    Time: 461 ms (29.49%) | Memory: 0B (100.00%) - LeetSync

--- my_leetcode/1258-article-views-i/article-views-i.sql
+++ my_leetcode/1258-article-views-i/article-views-i.sql
@@ -4,3 +4,8 @@ FROM Views
 WHERE author_id = viewer_id
 GROUP BY author_id
 ORDER BY author_id ASC;
+
+-- SELECT DISTINCT author_id AS id
+-- FROM Views
+-- WHERE author_id = viewer_id
+-- ORDER BY id;
