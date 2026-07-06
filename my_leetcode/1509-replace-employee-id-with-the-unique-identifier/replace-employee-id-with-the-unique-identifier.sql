9d6c919bfabfec0199c97d510aafeed776650fc3
Ajith Kumar Vishwakarma <akvk55555@gmail.com>
2026-07-06 18:20:31 +0530

    Time: 1373 ms (26.56%) | Memory: 0B (100.00%) - LeetSync

--- my_leetcode/1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ my_leetcode/1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -3,3 +3,9 @@ SELECT unique_id, name
 FROM Employees as e
 LEFT JOIN EmployeeUNI as uni
 ON e.id = uni.id;
+
+
+-- SELECT eu.unique_id, e.name
+-- FROM Employees e
+-- LEFT JOIN EmployeeUNI eu
+-- ON e.id = eu.id;
