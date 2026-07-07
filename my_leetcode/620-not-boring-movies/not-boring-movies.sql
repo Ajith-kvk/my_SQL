d1148a9459f9977c1511d8bc830e745a126cfa18
Ajith Kumar Vishwakarma <akvk55555@gmail.com>
2026-07-07 18:09:37 +0530

    Time: 241 ms (99.75%) | Memory: 0B (100.00%) - LeetSync

--- my_leetcode/620-not-boring-movies/not-boring-movies.sql
+++ my_leetcode/620-not-boring-movies/not-boring-movies.sql
@@ -1,5 +1,11 @@
 # Write your MySQL query statement below
-SELECT id, movie, description, rating
-FROM Cinema c
-WHERE c.id%2 != 0 AND c.description != 'boring'
+-- SELECT id, movie, description, rating
+-- FROM Cinema c
+-- WHERE c.id%2 != 0 AND c.description != 'boring'
+-- ORDER BY rating DESC;
+
+SELECT *
+FROM Cinema
+WHERE id % 2 = 1
+  AND description <> 'boring'
 ORDER BY rating DESC;
